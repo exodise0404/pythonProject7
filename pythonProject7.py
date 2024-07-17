51523de2fb9d3256c1645ccf9da09b88c199d715
exodise0404 <exodise0404@gmail.com>
2024-07-17 20:27:18 +0800

    Add files via upload

---
+++ pythonProject7.py
@@ -0,0 +1,12 @@
+my_dict = {'Vasya': 1975, 'Egor': 1999, 'Masha': 2002}
+print(my_dict)
+print(my_dict['Egor'])
+print(my_dict.get('Olga'))
+print(my_dict.pop('Masha'))
+my_dict.update({'Kamila': 1981, 'Artem': 1915})
+print(my_dict)
+my_set = {56,24,18,5,5,True,True,'Множество'}
+print(my_set)
+my_set.update({'Словарь',False})
+my_set.remove(True)
+print(my_set)
